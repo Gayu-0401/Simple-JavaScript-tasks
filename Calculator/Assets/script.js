0edecdecf7dae10549aac433f252467f2ff44184
GAYATHRI <135129516+Gayu-0401@users.noreply.github.com>
2024-10-29 20:41:24 +0530

    Add files via upload

---
+++ Calculator/Assets/script.js
@@ -0,0 +1,21 @@
+let input = document.getElementById("input");
+
+function display(num){
+    input .value += num;
+}
+function calculate(){
+    try{
+        input.value = eval(input.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+}
+function Clear(){
+    input.value = "";
+    
+}
+function del(){
+    input.value = input.value.slice(0,-1);
+}
